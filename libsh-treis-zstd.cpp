794d818413dc44c16d178890f02b4d2e193c6582
Askar Safin <safinaskar@mail.ru>
2020-08-23 14:43:38 +0300

    -

--- libsh-treis-zstd.cpp
+++ libsh-treis-zstd.cpp
@@ -2,4 +2,24 @@
 
 //@ #pragma once
 
+#include "libsh-treis/libsh-treis.hpp"
 #include "libsh-treis-zstd.hpp"
+
+//@ #include <span>
+//@ #include <cstddef>
+#include <zstd.h>
+namespace libsh_treis::zstd //@
+{ //@
+std::span<std::byte> //@
+x_compress (std::span<std::byte> dst, std::span<const std::byte> src, int compressionLevel)//@;
+{
+  std::size_t result = ZSTD_compress (dst.data (), dst.size (), src.data (), src.size (), compressionLevel);
+
+  if (ZSTD_isError (result))
+    {
+      _LIBSH_TREIS_THROW_MESSAGE (ZSTD_getErrorName (result));
+    }
+
+  return std::span<std::byte> (dst.data (), result);
+}
+} //@
